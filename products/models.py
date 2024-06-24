dcfee94dd8afc5a35e216b0bf254bff86e4e15f8
Monica Iancu <moniq.iancu@gmail.com>
2024-06-24 11:54:37 +0000

    Add products, model and fixtures

--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Product(models.Model):
+    category = models.ManyToManyField('Category', related_name='products')
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    ingredients = models.TextField()
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    nutritional_facts = models.JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
