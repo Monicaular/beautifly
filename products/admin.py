ab44b549efc838b70086ca9c587331b13afd4172
Monica Iancu <moniq.iancu@gmail.com>
2024-06-24 12:06:56 +0000

    Add customisation to admin panel for products & categories

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'get_categories',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku', )
+
+    def get_categories(self, obj):
+        return ", ".join([category.name for category in obj.category.all()])
+    get_categories.short_description = 'Categories'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
