738859d7cd9e92cb5c7d813d20f3a92e6a5a1829
Monica Iancu <moniq.iancu@gmail.com>
2024-06-29 13:27:19 +0000

    Set debug to false

--- wholefoods/settings.py
+++ wholefoods/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-monicaular-wholesomebas-d6fccy3ilk8.ws.codeinstitute-ide.net','.herokuapp.com']
 
