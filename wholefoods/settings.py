2348048bfcab67659970827d53483e47023e869c
Monica Iancu <moniq.iancu@gmail.com>
2024-06-30 10:48:40 +0000

    Add cache control settings

--- wholefoods/settings.py
+++ wholefoods/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = ['8000-monicaular-wholesomebas-d6fccy3ilk8.ws.codeinstitute-ide.net','.herokuapp.com']
 
@@ -157,6 +157,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+     # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Configuration
     AWS_STORAGE_BUCKET_NAME = 'wholeome-basket-e-commerce'
     AWS_S3_REGION_NAME = 'eu-west-2'
