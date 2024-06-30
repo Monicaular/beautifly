150b3149b80526169c77ad7c18a20388b585cdd2
Monica Iancu <moniq.iancu@gmail.com>
2024-06-30 15:26:25 +0000

    Add basket context and shipping logic

--- wholefoods/settings.py
+++ wholefoods/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'basket.contexts.basket_contents',
             ],
         },
     },
@@ -156,6 +157,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_SHIPPING_THRESHOLD = 30
+STANDARD_SHIPPING_PERCENTAGE = 10
 
 if 'USE_AWS' in os.environ:
      # Cache control
