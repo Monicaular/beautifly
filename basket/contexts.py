da116bad241ada16671fc75aec61dbf7a00f013f
Monica Iancu <moniq.iancu@gmail.com>
2024-07-07 08:58:08 +0000

    Fix bug in shipping calculations and add to basket

--- basket/contexts.py
+++ basket/contexts.py
@@ -9,12 +9,12 @@ def basket_contents(request):
     basket_total = Decimal('0.00')
     product_count = 0
     
-    for item_id, item_data in basket.items():
+    for item_key, item_data in basket.items():
         product = get_object_or_404(Product, pk=item_data['product_id'])
         num_units = item_data['quantity']
         size = item_data.get('size', '')
 
-        if product.has_multiple_sizes:
+        if product.has_multiple_sizes and size:
             price_for_size = product.get_price_for_quantity(size)
             if price_for_size is not None:
                 product_total = price_for_size * num_units
@@ -24,23 +24,23 @@ def basket_contents(request):
             product_total = product.fixed_size_price * num_units if product.fixed_size_price else product.price * num_units
 
 
-    basket_total += product_total
-    product_count += num_units
+        basket_total += product_total
+        product_count += num_units
 
-    basket_items.append({
-            'item_id': item_id,
-            'num_units': num_units,
-            'product': product,
-            'total_price': product_total,
-            'size': size,
-        })
-    if basket_total < settings.FREE_SHIPPING_THRESHOLD:
-        shipping = basket_total * Decimal(settings.STANDARD_SHIPPING_PERCENTAGE)
-        free_shipping_delta = settings.FREE_SHIPPING_THRESHOLD - basket_total
+        basket_items.append({
+                'item_id': item_key,
+                'num_units': num_units,
+                'product': product,
+                'total_price': product_total,
+                'size': size,
+            })
+    if basket_total < settings.FREE_SHIPPING_THRESHOLD and basket_total > Decimal('0.00'):
+        shipping = (basket_total * Decimal(settings.STANDARD_SHIPPING_PERCENTAGE) / Decimal('100')).quantize(Decimal('0.01'))
+        free_shipping_delta = (settings.FREE_SHIPPING_THRESHOLD - basket_total).quantize(Decimal('0.01'))
     else: 
         shipping = Decimal('0.00')
         free_shipping_delta = Decimal('0.00')
-    
+
     grand_total = basket_total + shipping
 
 
@@ -55,3 +55,4 @@ def basket_contents(request):
     }
 
     return context
+
--- basket/views.py
+++ basket/views.py
@@ -3,8 +3,6 @@ from django.http import HttpResponseBadRequest, HttpResponse
 from products.models import Product
 from decimal import Decimal
 
-
-
 def view_basket(request):
     """ A view that renders the basket content page """
 
@@ -18,37 +16,18 @@ def add_to_basket(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     size = request.POST.get('size')
     num_units = int(request.POST.get('num_units', 1))
-    redirect_url = request.POST.get('redirect_url')
-
-    if product.has_multiple_sizes:
-        quantity_to_kg = {
-            '100g': Decimal('0.1'),
-            '250g': Decimal('0.25'),
-            '1kg': Decimal('1'),
-        }
-
-        if size not in quantity_to_kg:
-            return redirect(redirect_url)
-
-        quantity_in_kg = quantity_to_kg[size]
-        total_price = round(product.price * Decimal(quantity_in_kg) * num_units, 2)
-
-    else:
-        quantity_in_kg = 1
-        total_price = product.fixed_size_price * num_units if product.fixed_size_price else product.price * num_units
+    redirect_url = request.POST.get('redirect_url', '/')
 
     basket = request.session.get('basket', {})
-    item_id = f"{item_id}-{size}"
 
-    if item_id in basket:
-        basket[item_id]['quantity'] += num_units
+    item_key = f"{item_id}-{size}" if size else str(item_id)
+
+    if item_key in basket:
+        basket[item_key]['quantity'] += num_units
     else:
-        basket[item_id] = {
+        basket[item_key] = {
             'product_id': product.id,
             'size': size,
-            'quantity_in_kg': quantity_in_kg,
-            'price_per_kg': str(product.price if product.has_multiple_sizes else ''),
-            'total_price': str(total_price),
             'quantity': num_units,
         }
 
