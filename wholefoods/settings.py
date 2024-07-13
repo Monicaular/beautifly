6bdd8241d598f0f6a3a7bc52f79a06c5c4fd964c
Monica Iancu <moniq.iancu@gmail.com>
2024-07-13 12:40:05 +0000

    Update basket context and delivery

--- wholefoods/settings.py
+++ wholefoods/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'basket.contexts.basket_contents',
+                'basket.contexts.basket_contents',
             ],
         },
     },
