2ddc45d85b7c27202826646a7e116ebccb657726
Monica Iancu <moniq.iancu@gmail.com>
2024-07-14 11:57:17 +0000

    Add toast messages to basket views

--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
+from django.contrib import messages
 from django.http import HttpResponse, JsonResponse
 from products.models import Product
 from decimal import Decimal
@@ -14,6 +15,7 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping basket """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
@@ -24,6 +26,8 @@ def add_to_basket(request, item_id):
         basket[item_id] = quantity
 
     request.session['basket'] = basket
+    messages.success(request, f'Added {quantity} x {product.name} to your basket')
+
     return redirect(redirect_url)
 
 from django.shortcuts import redirect, reverse
@@ -38,6 +42,7 @@ def adjust_basket(request, item_id):
         basket[item_id] = quantity
     else:
         basket.pop(item_id, None)
+        messages.success(request, 'Item removed from your basket')
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
@@ -51,9 +56,11 @@ def remove_from_basket(request, item_id):
         if str(item_id) in basket:
             del basket[str(item_id)]
             request.session['basket'] = basket
-            return JsonResponse({'success': True})
+            messages.success(request, 'Item removed successfully from basket.')
         else:
-            return JsonResponse({'success': False, 'error': 'Item not found in basket'}, status=404)
+            messages.error(request, 'Item not found in basket.')
 
     except Exception as e:
-        return JsonResponse({'success': False, 'error': str(e)}, status=500)
+        messages.error(request, f'Error removing item from basket: {str(e)}')
+
+    return redirect(reverse('view_basket'))
