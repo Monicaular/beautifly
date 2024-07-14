5e8abc96a1ef66ac3278853c78324247cc69d846
Monica Iancu <moniq.iancu@gmail.com>
2024-07-14 09:49:57 +0000

    Update adjust and remove from basket views and templates

--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
+from django.http import HttpResponse, JsonResponse
 from products.models import Product
 from decimal import Decimal
 
@@ -24,3 +25,35 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+from django.shortcuts import redirect, reverse
+
+def adjust_basket(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id, None)
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+def remove_from_basket(request, item_id):
+    """Remove the item from the shopping basket"""
+
+    try:
+        basket = request.session.get('basket', {})
+
+        if str(item_id) in basket:
+            del basket[str(item_id)]
+            request.session['basket'] = basket
+            return JsonResponse({'success': True})
+        else:
+            return JsonResponse({'success': False, 'error': 'Item not found in basket'}, status=404)
+
+    except Exception as e:
+        return JsonResponse({'success': False, 'error': str(e)}, status=500)
