68e7b4d738f445b2f3c8159d25fa83d188818904
Monica Iancu <moniq.iancu@gmail.com>
2024-07-16 15:51:07 +0000

    Add basic checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from basket.contexts import basket_contents
 from .forms import OrderForm
 
+import stripe
+
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "There's nothing in your basket at the moment")
@@ -11,9 +17,22 @@ def checkout(request):
 
     order_form = OrderForm()
 
+    # Retrieve basket items and calculate total quantity
     basket_items = basket_contents(request)['basket_items']
-    
     total_quantity = sum(item['quantity'] for item in basket_items)
+
+    # Calculate Stripe total amount (in cents) and create PaymentIntent
+    total_amount = int(basket_contents(request)['grand_total'] * 100)
+    stripe_total = int(total_amount * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
     
     context = {
         'basket_items': basket_items,
@@ -22,8 +41,8 @@ def checkout(request):
         'grand_total': basket_contents(request)['grand_total'],
         'order_form': order_form,
         'total_quantity': total_quantity,
-        'stripe_public_key': 'pk_test_51L7g7u2QlfePk4wlJYKjg6GUDBCFWIliokOfQYuqsh8YQTSxdfkerRXsCousl50sV8LT4piXLajpy63c5I3GVzB200uyEgU1PC',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, 'checkout/checkout.html', context)
