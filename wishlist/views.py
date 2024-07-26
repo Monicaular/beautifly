8eb41a06c686fb85f186e623c1af4b29cf61df3a
Monica Iancu <moniq.iancu@gmail.com>
2024-07-26 11:43:23 +0000

    Add append wishlist view and update template

--- wishlist/views.py
+++ wishlist/views.py
@@ -7,6 +7,27 @@ from products.models import Product
 @login_required
 def view_wishlist(request):
     """ A view that renders the wishlist content page """
+
     wishlist_items = Wishlist.objects.filter(user=request.user).select_related('product')
-    return render(request, 'wishlist/wishlist.html', {'wishlist_items': wishlist_items})
+
+    context = {
+        'wishlist_items': wishlist_items,
+    }
+    
+    return render(request, 'wishlist/wishlist.html', context)
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    """ Add a product to the user's wishlist """
+    product = get_object_or_404(Product, pk=product_id)
+    wishlist, created = Wishlist.objects.get_or_create(user=request.user, product=product)
+
+    if created:
+        messages.success(request, f'{product.name} has been added to your wishlist.')
+    else:
+        messages.info(request, f'{product.name} is already in your wishlist.')
+
+    return redirect(request.META.get('HTTP_REFERER', 'products'))
+
 
