741f728912f0c8f2c2018f4b2f3b607be61b84dd
Monica Iancu <moniq.iancu@gmail.com>
2024-07-27 10:43:17 +0000

    Add a 404 customised page

--- wholefoods/urls.py
+++ wholefoods/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404
+from django.conf.urls import handler500
+from .views import custom_404_page, custom_500_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,3 +17,6 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('wishlist/', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = custom_404_page
+handler500 = custom_500_page
