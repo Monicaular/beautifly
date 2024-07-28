3ec3926ed68f9974b9dcb1e489d4376ecdc92ce1
Monica Iancu <moniq.iancu@gmail.com>
2024-07-28 12:18:49 +0000

    Remove carousel from home page

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
-from .models import CarouselItem
 
-@admin.register(CarouselItem)
-class CarouselItemAdmin(admin.ModelAdmin):
-    list_display = ('caption_title', 'is_active')
-    list_filter = ('is_active',)
-    search_fields = ('image', 'caption_title', 'caption_text', 'title_link', 'text_link', 'is_active',)
 
 
--- homepage/views.py
+++ homepage/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render
-from .models import CarouselItem
+from django.shortcuts import render, redirect, get_object_or_404
 from products.models import Category
 
 
 def index(request):
-    carousel_items = CarouselItem.objects.filter(is_active=True)
     categories = Category.objects.exclude(name__in=['tofu', 'chilled', 'savoury_snacks', 'sweet_snacks', 'vegan_cheese', 'powders', 'flours_and_powders', 'pantry_staples', 'snacks_and_treats'])
 
     context = {
-        'carousel_items': carousel_items,
         'categories': categories,
     }
 
     
     return render(request, 'homepage/index.html', context)
+
