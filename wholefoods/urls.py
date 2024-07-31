074813080930f1109a84b7b47cda06efb93b9258
Monica Iancu <moniq.iancu@gmail.com>
2024-07-31 07:03:30 +0000

    Add shipping info and returns policy templates

--- wholefoods/urls.py
+++ wholefoods/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import handler404
 from django.conf.urls import handler500
+from django.views.generic import TemplateView
 from .views import custom_404_page, custom_500_page
 from . import views
 
@@ -12,6 +13,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about_us, name='about_us'),
     path('privacy-policy/', views.privacy_policy, name='privacy-policy'),
+    path('delivery-terms/', TemplateView.as_view(template_name='delivery_terms.html'), name='delivery_terms'),
+    path('quality_guarantee/', TemplateView.as_view(template_name='quality_guarantee.html'), name='quality_guarantee'),
     path('accounts/', include ('allauth.urls')),
     path('', include('homepage.urls')),
     path('products/', include ('products.urls')),
