5f1a1cb95332714ac5cb8c3777f42edea3373221
Monica Iancu <moniq.iancu@gmail.com>
2024-08-07 16:51:28 +0000

    Fix bug in checkout succes confirmation email

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -98,7 +98,6 @@ class StripeWH_Handler:
                     postcode__iexact=shipping_details.address.postal_code,
                     town_or_city__iexact=shipping_details.address.city,
                     street_address1__iexact=shipping_details.address.line1,
-                    street_address2__iexact=shipping_details.address.line2,
                     county__iexact=shipping_details.address.state,
                     grand_total=grand_total,
                     original_basket=basket,
@@ -118,6 +117,7 @@ class StripeWH_Handler:
             )
 
         else:
+            print('no order exists, creating one.')
             order = None
             try:
                 order = Order.objects.create(
