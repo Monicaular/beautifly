cc1129a5a23a23cef3d5a46fde18d3668d966dfe
Monica Iancu <moniq.iancu@gmail.com>
2024-08-17 10:54:29 +0000

    Update pagination display

--- products/forms.py
+++ products/forms.py
@@ -15,7 +15,7 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = "__all__"
+        exclude = ["rating"]
 
         image = forms.ImageField(
             label="Image", required=False, widget=CustomClearableFileInput
